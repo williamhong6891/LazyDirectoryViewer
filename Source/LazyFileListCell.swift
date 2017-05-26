56c54c440a1a2a07f22e58bf7ef34ac41751651b
Ho Lun Wan <hlwan@wonderfulworld.com>
2017-05-26 18:23:42 +0800

    Initial commit

---
+++ Source/LazyFileListCell.swift
@@ -0,0 +1,24 @@
+//
+//  LazyFileListCell.swift
+//  LazyDirectoryViewer
+//
+//  Created by Ho Lun Wan on 26/5/2017.
+//
+//
+
+import UIKit
+
+class LazyFileListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
